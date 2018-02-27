2ef8a2369a8f8cf634d6c1d54a456914418c5500
Nora Alosily <nemseo@gmail.com>
2018-02-27 08:53:11 -0600

    4th:     - Done with enum and genrating randoms.

--- app/src/main/java/edu/umsl/simonp1/GameModel.kt
+++ app/src/main/java/edu/umsl/simonp1/GameModel.kt
@@ -1,29 +1,50 @@
 package edu.umsl.simonp1
 
 import android.util.Log
+import java.util.Random;
 
 class GameModel(level: Level?) {
 
     var level: Level
     var levelName: String = level.toString()
     var initialLength: Int = 1
-    var speed: Int = 200
+    var duration: Int = 200
+    var sequence: ArrayList<Colors>? = null
 
 
     init {
         this.level = level ?: Level.EASY
+
         when(level){
             Level.EASY -> Log.e("Error", "easy")
-            Level.INTERMEDIATE -> setup(initialLength*3, speed/3)
+            Level.INTERMEDIATE -> setup(initialLength*3, duration /3)
             else -> setup(6,50 )
+        }
+
+
+        // todo: a. generate random number and add it to the seq, show it for the specified length
+        // todo: b. get the user seq, match it, and then fail or go back to a
+
+        sequence = ArrayList()
+
+        while( initialLength > 0 ){
+            val y :Colors? = getRandom()
+            sequence?.add(y!!)
+            initialLength--
         }
 
     }
 
-    private fun setup(initialLength: Int, speed: Int){
+    private fun setup(initialLength: Int, duration: Int){
         this.initialLength = initialLength
-        this.speed = speed
+        this.duration = duration
         Log.e("Error", levelName)
     }
+
+    private fun getRandom():Colors?{
+        var random: Random = Random()
+        return Colors.getColor(random.nextInt(4))
+    }
+
 
 }
--- app/src/main/java/edu/umsl/simonp1/Level.kt
+++ app/src/main/java/edu/umsl/simonp1/Level.kt
@@ -1,3 +1,16 @@
 package edu.umsl.simonp1
 
 enum class Level {EASY, INTERMEDIATE, DIFFICULT}
+
+enum class Colors(val color: Int){
+    BLUE(0),
+    GREEN(1),
+    RED(2),
+    YELLOW(3);
+
+    companion object {
+        private val map = Colors.values().associateBy(Colors::color)
+        fun getColor(value: Int) = map[value]
+    }
+}
+
